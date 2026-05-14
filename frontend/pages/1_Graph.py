189c4552083326dc6f234931df38dc43a082bc22
Mac-Alexander <mac-alexander@MacBook-Pro-Mac-Alexander-3.local>
2026-05-14 15:10:49 +0300

    add Iris

---
+++ frontend/pages/1_Graph.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title='Синусоида', layout='wide')
+
+with st.sidebar:
+    delta = st.slider('Delta', 1, 10, 2)
+
+col_1, col_2 = st.columns(2)
+
+x = np.linspace(0, 10, 100)
+y = np.sin(x) + delta
+
+df = pd.DataFrame({
+    'x': x,
+    'sin(x)': y
+})
+
+with col_1:
+    st.header('График: синусоида')
+    st.line_chart(df.set_index('x')['sin(x)'])
